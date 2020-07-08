9f3ac75125314e9526cb2732d58eb628bd8a9bd6
ebonynon <sithumsandeepap@gmail.com>
2020-07-08 16:58:10 +0530

    F2:0

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import { Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 // core components
-import Header from "../../components/Header/Header.js"; // ⛳
-import Footer from "../../components/Footer/Footer.js"; // ⛳
-import Parallax from "../../components/Parallax/Parallax.js";
+import Header from "./components/Header/Header.js"; // ⛳
+import Footer from "./components/Footer/Footer.js"; // ⛳
+import Parallax from "./components/Parallax/Parallax.js";
 
-import mainPageStyles from "../../assets/css/_views/mainPage.js";
+import mainPageStyles from "./assets/css/_views/mainPage.js";
 
 // Sections for this page
 import SearchSection from "./Sections/SearchSection.js";
@@ -23,13 +23,115 @@ import AddPartSection from "./Sections/AddPartSection.js";
 const useStyles = makeStyles(mainPageStyles);
 var hist = createBrowserHistory();
 
-export default function MainPage(props) {
-  const classes = useStyles();
+// export default function MainPage(props) {
+//   const classes = useStyles();
 
+//   return (
+//     <div>
+//       <Header />
+//       <Parallax filter image={require("./assets/img/Car-Parts.jpg")}>
+//         <div className={classes.container}>
+//           <Grid container>
+//             <Grid item xs={12} sm={12} md={6}>
+//               <Typography variant="h4">
+//                 There is sometimes an incorrect assumption that the parser
+//                 itself is what of ESLint with TypeScript.
+//               </Typography>
+//             </Grid>
+//           </Grid>
+//         </div>
+//       </Parallax>
+//       <div className={classNames(classes.main, classes.mainRaised)}>
+//         <div className={classes.container}>
+//           {/*<SearchSection />
+//            <br></br> // tmp tag for spacing
+//           <CardSection />
+//           <br></br> // tmp tag for spacing */}
+//           ReactDOM.render(
+//           <Router history={hist}>
+//             <Switch>
+//               <Route path="/add-part" component={AddPartSection} />
+//               <Route path="/" component={SearchSection} />
+//             </Switch>
+//           </Router>
+//           , document.getElementById("root") );
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default function MainPage(props) {
+//   const classes = useStyles();
+
+//   return (
+//     <div>
+//       <Header />
+//       <Parallax filter image={require("./assets/img/Car-Parts.jpg")}>
+//         <div className={classes.container}>
+//           <Grid container>
+//             <Grid item xs={12} sm={12} md={6}>
+//               <Typography variant="h4">
+//                 There is sometimes an incorrect assumption that the parser
+//                 itself is what of ESLint with TypeScript.
+//               </Typography>
+//             </Grid>
+//           </Grid>
+//         </div>
+//       </Parallax>
+//       <div className={classNames(classes.main, classes.mainRaised)}>
+//         <div className={classes.container}>
+//{
+/*<SearchSection />
+           <br></br> // tmp tag for spacing
+          <CardSection />
+          <br></br> // tmp tag for spacing */
+//}
+// ReactDOM.render(function MainPage(params) {
+//   const classes = useStyles();
+//   return (
+//     <div>
+//       <Header />
+//       <Parallax filter image={require("./assets/img/Car-Parts.jpg")}>
+//         <div className={classes.container}>
+//           <Grid container>
+//             <Grid item xs={12} sm={12} md={6}>
+//               <Typography variant="h4">
+//                 There is sometimes an incorrect assumption that the parser
+//                 itself is what of ESLint with TypeScript.
+//               </Typography>
+//             </Grid>
+//           </Grid>
+//         </div>
+//       </Parallax>
+//       <div className={classNames(classes.main, classes.mainRaised)}>
+//         <div className={classes.container}>
+//           <Router history={hist}>
+//             <Switch>
+//               <Route path="/add-part" component={AddPartSection} />
+//               <Route path="/" component={SearchSection} />
+//             </Switch>
+//           </Router>
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }, document.getElementById("root"));
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+function MainPage(params) {
+  const classes = useStyles();
   return (
     <div>
       <Header />
-      <Parallax filter image={require("../../assets/img/Car-Parts.jpg")}>
+      <Parallax filter image={require("./assets/img/Car-Parts.jpg")}>
         <div className={classes.container}>
           <Grid container>
             <Grid item xs={12} sm={12} md={6}>
@@ -43,21 +145,20 @@ export default function MainPage(props) {
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.container}>
-          {/*<SearchSection />
-           <br></br> // tmp tag for spacing
-          <CardSection />
-          <br></br> // tmp tag for spacing */}
-          ReactDOM.render(
           <Router history={hist}>
             <Switch>
               <Route path="/add-part" component={AddPartSection} />
               <Route path="/" component={SearchSection} />
             </Switch>
           </Router>
-          , document.getElementById("root") );
         </div>
       </div>
       <Footer />
     </div>
   );
 }
+
+ReactDOM.render(
+  <MainPage />,
+  document.getElementById("root")
+);
