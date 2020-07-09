ea429b535ac5d9d905c9404f2107c51eab8c07de
ebonynon <sithumsandeepap@gmail.com>
2020-07-09 07:58:51 +0530

    login register :P

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ import mainPageStyles from "./assets/css/_views/mainPage.js";
 import SearchSection from "./Sections/SearchSection.js";
 import AddPartSection from "./Sections/AddPartSection.js";
 import ShowPartSection from "./Sections/ShowPartSection.js";
+import Register from "./Sections/register.component.js";
+import Login from "./Sections/login.component.js";
 
 const useStyles = makeStyles(mainPageStyles);
 var hist = createBrowserHistory();
@@ -45,6 +47,8 @@ function MainPage(params) {
         <div className={classes.container}>
           <Router history={hist}>
             <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
               <Route path="/part/:id" component={ShowPartSection} />
               <Route path="/add-part" component={AddPartSection} />
               <Route path="/" component={SearchSection} />
